e92485f9c3a82416bb052af41dd7bc0e0f21bd2b
Dilyan Terziev <42419850+didorins@users.noreply.github.com>
2022-10-19 20:41:42 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 # Backend block pointing to s3. Must provision s3 manually, due to limitation.
 # TODO : inplement state locking with dynamoDB
 
-terraform {
-  backend "s3" {
-    bucket      = "exercisebucketdido"
-    key         = "terraform.state"
-    region      = "eu-west-3"
-  }
-}
+# terraform {
+#  backend "s3" {
+#    bucket      = "exercisebucketdido"
+#    key         = "terraform.state"
+#    region      = "eu-west-3"
+#  }
+#}
